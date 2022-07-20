c3ecb6123e3077bc4a211dde29dff2da92d4b8f7
Gonzalo Martin Campo <Gonzalo.m.martin@gmail.com>
2022-07-20 16:56:16 -0300

    Carrito check

--- JS/Index.js
+++ JS/Index.js
@@ -5,7 +5,7 @@ const lineasProductos = [ {
     descripcion:'El combo consta de un sillon esquinero de 2mt x 90cm x 160cm, mesa ratonera de 80cm x 60cm y 3 almohadones decorativos.',
     imgURL:"./Assets/IMG/Living.jpg",
     codigo: "Liv13",
-    cantidad: 5,
+    cantidad: 0,
 },
 {   nombre:'Dormitorio',
     parrafo: 'Descansa como se debe',
@@ -13,7 +13,7 @@ const lineasProductos = [ {
     descripcion:'El juego de dormitorio trae un sommier de 1.40cm X 1.90cm (dos plazas) junto a su respaldo correspondiente y dos mesas de luz de melamina',
     imgURL:'./Assets/IMG/Colchon.jpg', 
     codigo: "Dorm7",
-    cantidad: 5,
+    cantidad: parseInt("0"),
 },
 {   nombre:'Comedor Escandinavo',
     parrafo: 'El comedor que siempre quisiste por poco dinero!',
@@ -21,16 +21,17 @@ const lineasProductos = [ {
     descripcion:'Este set de comedor trae una mesa de vidrio con patas en madera paraiso de 1.60cm X 80cm junto a seis sillas tapizadas con patas en paraiso',
     imgURL:'./Assets/IMG/Comedor.jpg', 
     codigo: "Com20",
-    cantidad: 5,
+    cantidad: 0,
 }
 ];
 
+guardarProdLS(lineasProductos);
+
 const chango = [];
-const changuito = document.getElementById("chango");
+const changuitoHTML = document.getElementById("chango");
 
 
 const botonCliente = document.getElementById("BotonCliente");
-guardarProdLS(lineasProductos);
 
 botonCliente.onclick = () => { 
     let FondoBody = document.getElementById("Body");
@@ -40,14 +41,10 @@ botonCliente.onclick = () => {
     const divCartas = document.getElementById("CartasProds");
     divCartas.className = "BienvenidaCartas text-center row justify-content-center align-items-center";
     divCartas.style = "border: black 5px solid";
-    let product = recuperarProdLS();
-
-    renderProds(product, divCartas)
+    let lineas = recuperarProdLS();
+    
+    renderProds(lineas, divCartas)
 };
-
-
-
-
 
 const renderProds = (lineas, target) => {
     
@@ -72,6 +69,30 @@ const renderProds = (lineas, target) => {
     botonesCompra.forEach(button => button.addEventListener("click", handleClick));    
 }
 
+const handleClick = (event) => {
+    const codigo = event.target.getAttribute("ref");
+    const product = lineasProductos.find(linea => linea.codigo === codigo);
+    alert ("Acabas de comprar un " + product.nombre + " por $" + product.precio);
+
+    if (chango.some(el => el.codigo === product.codigo)) {
+        const posicionArray = chango.findIndex(el => el.codigo === product.codigo)
+        chango[posicionArray].cantidad = chango[posicionArray].cantidad + 1;
+    } else {
+        chango.push({
+            codigo : product.codigo,
+            nombre: product.nombre,
+            precio: product.precio,
+            cantidad: 1,
+            imgURL: product.imgURL,
+        })
+    }
+    guardarCarroSS(chango);
+
+    let changoSS = recuperarCarroSS();
+    renderChango(changoSS, changuitoHTML);
+    console.log(changoSS);
+}
+
 const renderChango = (lineas, target) => {
     let acumulador = '';
     lineas.map(producto => {
@@ -80,34 +101,18 @@ const renderChango = (lineas, target) => {
             <div class="card-body m-4">
                 <h5 class=" m-2 card-title">${producto.nombre}</h5>
                 <img src=${producto.imgURL} width="100" height="175" class="card-img-top" alt="${producto.nombre}">
-                <p class="mt-2 card-text"> Cantidad selecionada: ${producto.vendido} </br> Precio: $${producto.precio}.</p>
-                <button ref=${producto.codigo} class="boton_venta BCA btn btn-secondary my-2 col-md-3" id="botonCarroAgregar" > Agregar uno mas </button>
-                <button ref=${producto.codigo} class="boton_venta BCQ btn btn-secondary my-2 col-md-3" id="botonCarroQuitar" > Quitar uno </button>
+                <p class="mt-2 card-text"> Cantidad selecionada: ${producto.cantidad} </br> Precio: $${producto.precio} </br> Precio Total: $${producto.precio * producto.cantidad}.</p>
+                <div class="row justify-content-around">
+                    <button ref=${producto.codigo} class="boton_venta BCA btn btn-secondary my-2 col-md-3" id="botonCarroAgregar" > Agregar uno mas </button>
+                    <button ref=${producto.codigo} class="boton_venta BCQ btn btn-secondary my-2 col-md-3" id="botonCarroQuitar" > Quitar uno </button>
+                </div>
             </div>
         </div>   
         `
     })
 
     target.innerHTML = acumulador;   
-}
 
-const handleClick = (event) => {
-    const cod = event.target.getAttribute("ref");
-    const code = lineasProductos.find(prod => prod.codigo === cod);
-    if (chango.some(compra => compra.codigo === code.codigo)) {
-        const posicion = chango.findIndex(el => el.codigo === code.codigo)
-        chango[posicion].vendido = chango[posicion].vendido + 1 ;
-        } else {
-            chango.push({
-                codigo:code.codigo,
-                nombre: code.nombre,
-                precio: code.precio,
-                imgURL: code.imgURL,
-                vendido: code.vendido, 
-                }
-            )   
-        }
-    renderChango (chango, changuito);
 }
 
 function guardarCarroSS(carro) {
